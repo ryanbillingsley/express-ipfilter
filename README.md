cec9e3694eb3181d51e7bd284a7c001072184508
Michael Schonfeld <michael@dwolla.com>
2012-08-09 15:46:38 -0400

    First push

---
+++ README.md
@@ -0,0 +1,53 @@
+IP Filter: A light-weight IP address based connection filtering system
+=================================================================================
+
+## Version
+0.0.3
+
+## Requirements
+- [Node](http://github.com/ry/node)
+
+## Installation
+
+Recommended installation is with npm. To add express-csrf to your project, do:
+
+    npm install ipfilter
+
+## Usage
+
+    var express = require('express'),
+        ipfilter = require('ipfilter'),
+        ips = ['127.0.0.1'];
+
+    app = express.createServer();
+    app.use(ipfilter(ips));
+    app.listen(3000);
+
+## Credits
+
+Michael Schonfeld &lt;michael@dwolla.com&gt;
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2011 Dwolla &lt;michael@dwolla.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ ipfilter.js
@@ -0,0 +1,89 @@
+/*!
+ * Dwolla - IP Filter
+ * Copyright(c) 2011 Dwolla Inc.
+ * MIT Licensed
+ */
+
+
+/**
+ * Module dependencies.
+ */
+var _ = require('underscore');
+
+
+/**
+ * IP Filter:
+ *
+ * IP Filtering middleware; 
+ *
+ * Examples:
+ *
+ *      var ipfilter = require('ipfilter'),
+ *          ips = ['127.0.0.1'];
+ *
+ *      app.use(ipfilter(ips));
+ *
+ * Options:
+ *
+ *  - `mode` whether to deny or grant access to the IPs provided . Defaults to 'deny'.
+ *  - `log` console log actions. Defaults to true.
+ *  - `errorCode` the HTTP status code to use when denying access. Defaults to 401.
+ *  - `errorMessage` the error message to use when denying access. Defaults to 'Unauthorized'.
+ *
+ * @param [Array] IP addresses
+ * @param {Object} options
+ * @api public
+ */
+module.exports = function ipfilter(ips, opts) {
+	var ips = ips || false
+		, opts = _.extend({
+			mode: 'deny'
+			, log: true
+			, errorCode: 401
+			, errorMessage: 'Unauthorized'
+		}, opts)
+		, getClientIp = function(req) {
+			var ipAddress;
+
+			var forwardedIpsStr = req.header('x-forwarded-for'); 
+
+			if (forwardedIpsStr) {
+				var forwardedIps = forwardedIpsStr.split(',');
+				ipAddress = forwardedIps[0];
+			}
+
+			if (!ipAddress) {
+				ipAddress = req.connection.remoteAddress;
+			}
+
+			return ipAddress;
+		}
+		;
+
+	return function(req, res, next) {
+		// If no IPs were specified, skip
+		// this middleware
+		if(!ips || !ips.length) { return next(); }
+
+		var ip = getClientIp(req) // Grab the client's IP address
+			, mode = opts.mode.toLowerCase() // Normalize mode
+			;
+
+		if((mode == 'allow' && ips.indexOf(ip) !== -1) || (mode == 'deny' && ips.indexOf(ip) === -1)) {
+			// Grant access
+			if(opts.log) {
+				console.log('Access granted to IP address: ' + ip);
+			}
+
+			return next();
+		}
+
+		// Deny access
+		if(opts.log) {
+			console.log('Access denied to IP address: ' + ip);
+		}
+
+		res.statusCode = opts.errorCode;
+		return res.end(opts.errorMessage);
+	}
+};
